2567d5f7a4caa692eb12eafcac5db1224a9aefe1
sonik91 <59109698+sonik91@users.noreply.github.com>
2021-04-02 16:28:16 +0200

    Update config.js

--- Class/config.js
+++ Class/config.js
@@ -2,8 +2,8 @@ const config = {
     "cortex" : {
         "socketUrl" : 'wss://localhost:6868',
         "user" : {//info api cree sur : https://www.emotiv.com/my-account/cortex-apps/
-            "clientId":"l2PKZzfjiaNeViM48P2ei37iybrgj8h1i5ROajkR",
-            "clientSecret":"PmgFBidEkOkrwseyOnTaRnSUZr5ifwFRC9R9q2MC9r7J8wSe8LdFaGAdjxD8bP4t9bvAjzRiU3NWYYS37bY7hM7yVcRC45mx042tmGvSGZqqOoYqqvEjk10OPV5ygSiT",
+            "clientId":"your clientID",
+            "clientSecret":"yourClientSecret",
             "debit":2
         }
     },
